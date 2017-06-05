af49ff80f3ce8b0f734374430e48bb1d723593e8
Samuel Lair <lair001@gmail.com>
2017-06-05 18:20:32 -0400

    Done.

--- index.js
+++ index.js
@@ -10,17 +10,56 @@ function buildHistogram(magazine) {
 	return histogram;
 }
 
+function buildHistogramUntil(char, histogram, magazine, startingIndex) {
+	var letter,
+		i = startingIndex;
+	for (i = startingIndex; i < magazine.length; i++) {
+		letter = magazine[i];
+		if (letter === char) {
+			return [true, i + 1];
+		} else {
+			if (!histogram[letter]) {
+				histogram[letter] = 1;
+			} else {
+				histogram[letter]++;
+			}
+		}
+	}
+	return [false, i];
+}
+
 function canBuildNote(magazine, note) {
-	var histogram = buildHistogram(magazine),
+	var histogram = {},
 		noteChars = note.split(''),
-		canBuild = true;
+		canBuild = true,
+		buildResult,
+		startingIndex = 0;
 	noteChars.forEach(function(char) {
 		if (histogram[char]) {
 			histogram[char]--;
 		} else {
-			canBuild = false;
-			return;
+			buildResult = buildHistogramUntil(char, histogram, magazine, startingIndex);
+			canBuild = buildResult[0];
+			startingIndex = buildResult[1];
+			if (!canBuild) {
+				return;
+			}
 		}
 	});
 	return canBuild;
 }
+
+// function canBuildNote(magazine, note) {
+// 	var histogram = buildHistogram(magazine),
+// 		noteChars = note.split(''),
+// 		canBuild = true;
+// 	noteChars.forEach(function(char) {
+// 		if (histogram[char]) {
+// 			histogram[char]--;
+// 		} else {
+// 			canBuild = false;
+// 			return;
+// 		}
+// 	});
+// 	return canBuild;
+// }
