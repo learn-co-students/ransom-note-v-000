5847e3c48a284169522324071c4997b9b6a859d0
github username <github email address>
2018-02-27 21:35:43 -0500

    Done.

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ function buildHistogram(magazine) {
 function canBuildNote(magazine, note) {
   let histogram = buildHistogram(magazine);
   const length = note.length;
+
   for (let i = 0; i < length; i++) {
     if (histogram[note[i]] && histogram[note[i]] > 0) {
       histogram[note[i]] -= 1;
-      console.log(note[i]);
     } else {
       return false;
     }
   }
+  
   return true;
 }
