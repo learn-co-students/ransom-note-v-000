7501c4ec7cca5265e15958f20c99e74b9db9e867
rcrdlbl <bluh@outlook.com>
2019-05-20 22:50:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function buildHistogram(array) {
-  
+  let histogram = {}
+  array.forEach(function(element) {
+    if (histogram[element]) {
+      histogram[element] += 1
+    } else {
+      histogram[element] = 1
+    }
+  })
+  return histogram
 }
